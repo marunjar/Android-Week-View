f37edb3b834780529c6ce0c31f3936ad630c804b
Andrius-B <drugelissp@gmail.com>
2018-04-02 22:00:51 +0300

    Fixed scroll input lag

--- library/src/main/java/com/alamkanak/weekview/WeekView.java
+++ library/src/main/java/com/alamkanak/weekview/WeekView.java
@@ -190,8 +190,15 @@ public class WeekView extends View {
 
         @Override
         public boolean onDown(MotionEvent e) {
-            goToNearestOrigin();
+            stopScrolling();
+            //goToNearestOrigin();
             return true;
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e){
+            goToNearestOrigin();
+            return false;
         }
 
         @Override
@@ -298,6 +305,7 @@ public class WeekView extends View {
 
         @Override
         public boolean onSingleTapConfirmed(MotionEvent e) {
+            goToNearestOrigin();
 
             // If the tap was on an event then trigger the callback.
             if (mEventRects != null && mEventClickListener != null) {
@@ -397,6 +405,7 @@ public class WeekView extends View {
         @Override
         public void onLongPress(MotionEvent e) {
             super.onLongPress(e);
+            goToNearestOrigin();
 
             if (mEventLongPressListener != null && mEventRects != null) {
                 List<EventRect> reversedEventRects = mEventRects;
@@ -2537,6 +2546,16 @@ public class WeekView extends View {
         }
 
         return val;
+    }
+
+    /**
+     * A lighter function to stop the current scroll animation
+     */
+    private void stopScrolling(){
+        //force scroller animation stop
+        mScroller.forceFinished(true);
+        // Reset scrolling and fling direction.
+        mCurrentScrollDirection = mCurrentFlingDirection = Direction.NONE;
     }
 
     private void goToNearestOrigin() {
